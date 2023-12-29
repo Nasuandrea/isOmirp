eacdb8a302fadd2c811665b6f3a79aacca0f4113
nasua <acollazocacho@gmail.com>
2023-12-29 12:41:23 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,36 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+// Ejercicio 9 - Omirp
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println(isOmirp(100));
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    }
+    public static Boolean isPrime(int numero){
+        if (numero < 2) {
+            return false;
         }
+        for (int n = 2; n <= Math.sqrt(numero); n++) {
+            if (numero % n == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean isOmirp (int numero){
+        String number = String.valueOf(numero);
+        int cont=0;
+        char[] digitos = number.toCharArray();
+        char[] digitos2 =new char [digitos.length];
+        //intento de invertir el array de digitos
+        for (int i = digitos.length-1; i==0; i--) {
+            digitos2[cont] = digitos[i];
+            System.out.println(digitos2[cont]);
+            //esto no imprime nada no se porque
+            cont++;
+        }
+
+
+        return true;
     }
 }
